1ccc7ec30ee56b2d27e291c69998c2b061c1d62c
Jose Ángel Gumiel <jagumiel001@gmail.com>
2019-01-25 15:38:05 +0100

    SoC. FPGA running an ADC and HPS reading and printing the values.
    The FPGA has a module to configure and read an ADC.
    The HPS is collecting the data and printing on screen.
    
    It's just a test to see how it works and to learn in order to develop more complex prototypes.

---
+++ VHDL/HPS-FPGA-SoC/hps_adc/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include "hps_0.h"
+
+#define REG_BASE 0xff200000
+#define REG_SPAN 0x00200000
+
+void *base;
+uint32_t *adc;
+int fd;
+
+void handler(int signo){
+	*adc=0;
+	munmap(base, REG_SPAN);
+	close(fd);
+	exit(0);
+}
+
+int main(){
+	int value=0;
+	int last_value=0;
+	fd=open("/dev/mem",(O_RDWR|O_SYNC)); //Abro la memoria del sistema.
+	if(fd<0){
+		printf("Can't open memory. \n");
+		return -1;
+	}
+	base=mmap(NULL,REG_SPAN,(PROT_READ|PROT_WRITE),MAP_SHARED,fd,REG_BASE);
+	if(base==MAP_FAILED){
+		printf("Can't MAP memory. \n");
+		close(fd);
+		return -1;
+	}
+	adc=(uint32_t*)(base+ADC_VAL_BASE); //El ADC_VAL_BASE esta en la cabecera.
+	signal(SIGINT, handler);
+	//printf("El valor actual es: /n");
+	while(1){
+		value=*adc;
+		printf("El valor actual es: %d\n", *adc);
+		usleep(250000);
+	}
+}
---
+++ VHDL/HPS-FPGA-SoC/hps_adc/output_files/sof_to_rbf.bat
@@ -0,0 +1,2 @@
+%QUARTUS_ROOTDIR%\\bin64\\quartus_cpf -c -o bitstream_compression=on hps_adc.sof adc_soc_system.rbf
+pause
