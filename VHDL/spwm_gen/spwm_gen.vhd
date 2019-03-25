01cd2e1ae18415e6b653f21b8078786060f135a1
unknown <jagumiel001@gmail.com>
2019-03-25 18:37:02 +0100

    Update: Delay added

--- VHDL/spwm_gen/spwm_gen.vhd
+++ VHDL/spwm_gen/spwm_gen.vhd
@@ -7,9 +7,12 @@ entity spwm_gen is
 		clk	: in  std_logic;
 --		freq_sin: std_logic_vector(31 downto 0);
 --		freq_tri: std_logic_vector(31 downto 0);
-		phase1 	: out std_logic;
-		phase2 	: out std_logic;
-		phase3 	: out std_logic
+		phase1a: out std_logic;
+		phase2a: out std_logic;
+		phase3a: out std_logic;
+		phase1b: out std_logic;
+		phase2b: out std_logic;
+		phase3b: out std_logic
 	);
 end spwm_gen;
 
@@ -18,37 +21,37 @@ architecture a of spwm_gen is
 --Componentes:
 component triangle
 	port (	-- system signals
-		clk:		in	std_logic;
-		amp:		in	std_logic_vector(32-1 downto 0);
-		frq:		in	std_logic_vector(32-1 downto 0);
-		pwm_ctrl:	out	std_logic_vector(32-1 downto 0)
+		clk:		in	 std_logic;
+		amp:		in	 std_logic_vector(32-1 downto 0);
+		frq:		in	 std_logic_vector(32-1 downto 0);
+		pwm_ctrl:out std_logic_vector(32-1 downto 0)
 	);
 end component;
 
 component sin
 	port (
-		clk:		in	std_logic;
-		amp:		in	std_logic_vector(32-1 downto 0);
-		frq:		in	std_logic_vector(32-1 downto 0);
-		pwm_ctrl:	out	std_logic_vector(32-1 downto 0)
+		clk:		in	 std_logic;
+		amp:		in	 std_logic_vector(32-1 downto 0);
+		frq:		in	 std_logic_vector(32-1 downto 0);
+		pwm_ctrl:out std_logic_vector(32-1 downto 0)
 	);
 end component;
 
 component sin_120
 	port (
-		clk:		in	std_logic;
-		amp:		in	std_logic_vector(32-1 downto 0);
-		frq:		in	std_logic_vector(32-1 downto 0);
-		pwm_ctrl:	out	std_logic_vector(32-1 downto 0)
+		clk:		in  std_logic;
+		amp:		in	 std_logic_vector(32-1 downto 0);
+		frq:		in  std_logic_vector(32-1 downto 0);
+		pwm_ctrl:out std_logic_vector(32-1 downto 0)
 	);
 end component;
 
 component sin_240
 	port (
-		clk:		in	std_logic;
-		amp:		in	std_logic_vector(32-1 downto 0);
-		frq:		in	std_logic_vector(32-1 downto 0);
-		pwm_ctrl:	out	std_logic_vector(32-1 downto 0)
+		clk:		in	 std_logic;
+		amp:		in	 std_logic_vector(32-1 downto 0);
+		frq:		in	 std_logic_vector(32-1 downto 0);
+		pwm_ctrl:out std_logic_vector(32-1 downto 0)
 	);
 end component;
 
@@ -66,41 +69,41 @@ begin
 
 	inst1: triangle
 	port map(
-		clk         => clk,
-		amp	    => x"FFFFFFFF",
-		--frq         => x"000003E8",--50kHz
-		--frq	    => x"000001F4", --100kHz
-		--frq	    => x"00001388", --10kHz
-		frq	    => x"000009C4", --20kHz
-		pwm_ctrl    => triangular
+		clk		=> clk,
+		amp		=> x"FFFFFFFF",
+		--frq		=> x"000003E8",--50kHz
+		--frq		=> x"000001F4", --100kHz
+		--frq		=> x"00001388", --10kHz
+		frq		=> x"000009C4", --20kHz
+		pwm_ctrl	=> triangular
 	);
 
 	inst2: sin
 	port map(
-		clk         => clk,
-		amp	    => x"FFFFFFFF",
-		--frq         => x"00001388", --Supuestamente, 5000 ciclos de 20ns, 10kHz
-		frq         => x"0000C350", --Supuestamente, 50000 ciclos de 20ns, 1kHz
-		pwm_ctrl    => seno
+		clk		=> clk,
+		amp		=> x"FFFFFFFF",
+		--frq		=> x"00001388", --Supuestamente, 5000 ciclos de 20ns, 10kHz
+		frq		=> x"0000C350", --Supuestamente, 50000 ciclos de 20ns, 1kHz
+		pwm_ctrl	=> seno
 	);
 
 	inst3: sin_120
 	port map(
-		clk         => clk,
-		amp	    => x"FFFFFFFF",
-		frq         => x"0000C350", --Supuestamente, 5000 ciclos de 20ns, 10kHz
-		pwm_ctrl    => seno120
+		clk		=> clk,
+		amp		=> x"FFFFFFFF",
+		frq		=> x"0000C350", --Supuestamente, 5000 ciclos de 20ns, 10kHz
+		pwm_ctrl	=> seno120
 	);
 
 	inst4: sin_240
 	port map(
-		clk         => clk,
-		amp	    => x"FFFFFFFF",
-		frq         => x"0000C350", --Supuestamente, 5000 ciclos de 20ns, 10kHz
-		pwm_ctrl    => seno240
+		clk		=> clk,
+		amp		=> x"FFFFFFFF",
+		frq		=> x"0000C350", --Supuestamente, 5000 ciclos de 20ns, 10kHz
+		pwm_ctrl	=> seno240
 	);
 
-	process(clk)
+	process(clk)--Ojo, igual tienes que anadir las variables que lees a la lista de sensibilidad
 	begin
 		if(triangular<seno)then
 			pulso<='0';
@@ -120,7 +123,10 @@ begin
 			pulso240<='1';
 		end if;
 	end process;
-	phase1<=pulso;
-	phase2<=pulso120;
-	phase3<=pulso240;
+	phase1a<=pulso;
+	phase2a<=pulso120;
+	phase3a<=pulso240;
+	phase1b<=not(pulso);
+	phase2b<=not(pulso120);
+	phase3b<=not(pulso240);
 end a;
